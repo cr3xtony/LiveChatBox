e3506f91f808c14fa0c0832309cf57bb67c8ab70
cr3xtony <sumonroytony@gmail.com>
2021-03-10 22:10:28 +0600

    not final version

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
 import HomeScreen from './screens/HomeScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import LoginScreen from './screens/LoginScreen';
@@ -10,16 +9,14 @@ import ChatScreen from './screens/ChatScreen';
 function App() {
   return (
     <Router forceRefresh={true}>
-      <Container className="container">
-        <Route path="/login" component={LoginScreen} exact />
-        <Route path="/register" component={RegisterScreen} exact />
-        <Route
-          path="/chatroom/:id"
-          render={(props) => <ChatScreen {...props} />}
-        />
+      <Route path="/login" component={LoginScreen} exact />
+      <Route path="/register" component={RegisterScreen} exact />
+      <Route
+        path="/chatroom/:id"
+        render={(props) => <ChatScreen {...props} />}
+      />
 
-        <Route path="/" component={HomeScreen} exact />
-      </Container>
+      <Route path="/" component={HomeScreen} exact />
     </Router>
   );
 }
